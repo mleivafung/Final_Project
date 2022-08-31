b5fe919db803ea2d1ecd4f874161b5663f33ae26
Manuel Leiva Fung <99558882+mleivafung@users.noreply.github.com>
2022-08-31 10:01:26 -0500

    Delete secret.py

--- secret.py
+++
@@ -1,7 +0,0 @@
-apiKey = '2cac432e'
-
-link1 = 'https://www.boxofficemojo.com/chart/top_lifetime_gross/?area=XWW'
-link2 = 'https://www.boxofficemojo.com/chart/top_lifetime_gross/?area=XWW&offset=200'
-link3 = 'https://www.boxofficemojo.com/chart/top_lifetime_gross/?area=XWW&offset=400'
-link4 = 'https://www.boxofficemojo.com/chart/top_lifetime_gross/?area=XWW&offset=600'
-link5 = 'https://www.boxofficemojo.com/chart/top_lifetime_gross/?area=XWW&offset=800'
